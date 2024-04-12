cfaa27be74a4fe150965b64f1f0001065e5b9e06
munyi <43235624+munyiwamicha@users.noreply.github.com>
2024-04-13 02:34:36 +0300

    changes

---
+++ app/Models/ProductModel.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductModel extends Model
+{
+    use HasFactory;
+    protected $table = "product";
+    protected $fillable = [
+
+        "name",
+        "description",
+        "skuNumber",
+        "category",
+        "supplier",
+        "numberAvailable",
+        "price"
+    ] ;
+}
