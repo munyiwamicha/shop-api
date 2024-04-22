47e87e0f0c60c9d3c368e855848a6d47e935672c
munyi <43235624+munyiwamicha@users.noreply.github.com>
2024-04-22 17:29:12 +0300

    add passport authentication

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SupplierController;
 
@@ -34,8 +33,6 @@ Route::put('/supplier', [SupplierController::class, 'updateSupplier']);
 Route::delete('/supplier', [SupplierController::class, 'deleteSupplier']);
 
 
-=======
->>>>>>> main
 
 Route::get('/user', function (Request $request) {
     return $request->user();
